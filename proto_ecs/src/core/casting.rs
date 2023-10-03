4850b217f7dd8cdaf1f2e9052ef653870fb0103e
Luis Diaz <ldiazn98@gmail.com>
2023-10-02 22:02:22 -0400

    added safe cast versions of cast macros

--- proto_ecs/src/core/casting.rs
+++ proto_ecs/src/core/casting.rs
@@ -20,3 +20,17 @@ macro_rules! cast_mut {
         $v.as_any_mut().downcast_mut::<$t>().expect("Cast is not possible")
     };
 }
+
+#[macro_export]
+macro_rules! safe_cast {
+    ($v:ident, $t:ident) => {
+        $v.as_any().downcast_ref::<$t>()
+    };
+}
+
+#[macro_export]
+macro_rules! safe_cast_mut {
+    ($v:ident, $t:ident) => {
+        $v.as_any_mut().downcast_mut::<$t>()
+    };
+}
