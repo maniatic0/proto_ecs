f6c5adc4ec38be2ee8e7ff0fd573301c45a4a76e
Christian Oliveros <christianol_01@hotmail.com>
2023-10-18 22:37:59 +0200

    Basic Entity creation

--- proto_ecs/src/entities/entity.rs
+++ proto_ecs/src/entities/entity.rs
@@ -1,10 +1,11 @@
 use crate::{
-    data_group::{DataGroup, DataGroupID},
+    data_group::{DataGroup, DataGroupID, DataGroupInitType, DataGroupRegistry},
     entities::entity_spawn_desc::EntitySpawnDescription,
+    local_systems::LocalSystemRegistry,
 };
 use proto_ecs::local_systems::{StageID, SystemClassID, SystemFn, STAGE_COUNT};
 
-use bitvec::prelude::BitArr;
+use bitvec::prelude::{BitArr, BitArray};
 use nohash_hasher::IntSet;
 use vector_map::{set::VecSet, VecMap};
 
@@ -44,7 +45,7 @@ pub struct Entity {
 }
 
 impl Entity {
-    pub fn new(id: EntityID, spawn_desc: EntitySpawnDescription) -> Self {
+    pub fn init(id: EntityID, spawn_desc: EntitySpawnDescription) -> Self {
         let EntitySpawnDescription {
             name,
             debug_info,
@@ -54,14 +55,66 @@ impl Entity {
             children,
         } = spawn_desc;
 
+        let dg_registry = DataGroupRegistry::get_global_registry().read();
+        let mut datagroups = DataGroupMap::new();
+
+        for (id, init_params) in data_groups {
+            let entry = dg_registry.get_entry_by_id(id);
+
+            let mut new_dg = (entry.factory_func)();
+
+            match init_params {
+                DataGroupInitType::Uninitialized(msg) => {
+                    panic!("Uninitialized DataGroup '{}': {msg}", entry.name);
+                }
+                DataGroupInitType::NoInit => (),
+                DataGroupInitType::NoArg => new_dg.__init__(None),
+                DataGroupInitType::Arg(param) => new_dg.__init__(Some(param)),
+                DataGroupInitType::OptionalArg(param) => new_dg.__init__(param),
+            }
+
+            datagroups.insert(id, new_dg);
+        }
+
+        let mut stage_enabled_map = BitArray::ZERO;
+        let mut stage_map = StageMap::new();
+
+        let ls_registry = LocalSystemRegistry::get_global_registry().read();
+        for id in &local_systems {
+            let entry = ls_registry.get_entry_by_id(*id);
+
+            entry
+                .functions
+                .iter()
+                .enumerate()
+                .for_each(|(stage_id, fun)| {
+                    let stage_id = stage_id as StageID;
+                    match fun {
+                        None => (),
+                        Some(fun) => {
+                            if !stage_enabled_map[stage_id as usize] {
+                                stage_enabled_map.set(stage_id as usize, true);
+                                stage_map.insert(stage_id, Vec::new());
+                            }
+
+                            let stage = stage_map.get_mut(&stage_id).unwrap();
+                            stage.push(*fun);
+                        }
+                    }
+                });
+        }
+
+        // TODO: Add datagroup index math here and to the struct
+        // TODO: Add local system sorting here
+
         Self {
             id,
             name,
             debug_info,
-            datagroups: todo!(),
-            local_systems_map: todo!(),
-            stage_enabled_map: todo!(),
-            stage_map: todo!(),
+            datagroups,
+            local_systems_map: local_systems,
+            stage_enabled_map,
+            stage_map,
             parent,
             children,
         }
@@ -144,5 +197,10 @@ impl Entity {
             self.is_stage_enabled(stage_id),
             "Check if the stage is enabled before running it!"
         );
+
+        let stage = self
+            .stage_map
+            .get_mut(&stage_id)
+            .expect("Unitialized Entity or Entity in undefined state!");
     }
 }
