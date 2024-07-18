5814991423f8ffcdd923c7209d4bb15b6d03ae93
Luis Diaz <41093870+LDiazN@users.noreply.github.com>
2024-07-17 21:55:50 -0400

    Add basic event types

--- proto_ecs/src/core/events.rs
+++ proto_ecs/src/core/events.rs
@@ -2,11 +2,26 @@
 /// but translated to this canonical Event data types to abstract platform-specific APIs
 
 
+#[derive(Debug)]
 pub struct Event {
     handled : bool,
     event_type : Type
 }
 
+#[derive(Debug)]
 pub enum Type {
-    
+    WindowClose, WindowResize, WindowFocus, WindowLostFocus, WindowMoved,
+    AppTick, AppUpdate, AppRender, 
+    KeyPressed, KeyReleased, KeyTyped,
+    MouseButtonPressed, MouseButtonReleased, MouseMoved, MouseScrolled
+}
+
+impl Event {
+    fn is_handled(&self) -> bool {
+        self.handled
+    }
+
+    fn make_handled(&mut self) {
+        self.handled = true
+    }
 }
