9f6f4f8ef9bbe6a3a51cff1295d3ac2608dc6017
Luis Diaz <41093870+LDiazN@users.noreply.github.com>
2024-07-21 00:18:34 -0400

    Testing builder pattern in window manager initializer

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,28 +1,26 @@
-use proto_ecs::{core::window::{Platforms, WindowBuilder}, prelude::*};
+use proto_ecs::prelude::*;
 
 struct MyLayer;
 
-impl  Layer for MyLayer {
-    fn on_attach(&mut self) {
-        println!("Attaching my layer!")
-    }
+impl Layer for MyLayer {
+    fn on_attach(&mut self) {}
 
-    fn on_detach(&mut self) {
-        println!("Detaching my layer")
-    }
+    fn on_detach(&mut self) {}
 
-    fn update(&mut self, delta_time: f32) {
-        println!("Updating updating")
-    }
+    fn update(&mut self, delta_time: f32) {}
 
-    fn on_event(&mut self, event: &mut Event) {
-        println!("Handling event")
-    }
+    fn on_event(&mut self, event: &mut Event) {}
 }
 
 fn main() {
     App::initialize();
-    WindowManager::init(WindowBuilder::new(), Platforms::Windows);
+    WindowManager::init(
+        WindowBuilder::new()
+            .with_height(300)
+            .with_width(600)
+            .with_title("Sandbox Testing".to_owned()),
+        Platforms::Windows,
+    );
 
     App::add_layer(Box::new(MyLayer));
 
