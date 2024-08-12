d27fb2b455564e34deb7c7dd044f5737a293738e
Luis Diaz <41093870+LDiazN@users.noreply.github.com>
2024-08-12 17:52:57 -0400

    Sample code to create an imgui window

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -7,10 +7,12 @@ use proto_ecs::core::render::render_api::RenderCommand;
 use proto_ecs::core::render::shader::{create_shader, ShaderDataType, ShaderPtr};
 use proto_ecs::core::render::vertex_array::{create_vertex_array, VertexArrayPtr};
 use proto_ecs::prelude::*;
+use imgui;
 
 struct MyLayer {
     triangle_shader: Option<RwLock<ShaderPtr>>,
     triangle_data: Option<RwLock<VertexArrayPtr>>,
+    color : glam::Vec3,
 }
 
 // TODO Look for something to do in these cases
@@ -20,6 +22,7 @@ unsafe impl Sync for MyLayer {}
 const VERTEX_SRC: &str = "
 #version 100
 precision mediump float;
+uniform vec3 u_Color;
 
 attribute vec2 position;
 attribute vec3 color;
@@ -35,11 +38,11 @@ void main() {
 const FRAGMENT_SRC: &str = "
 #version 100
 precision mediump float;
-
+uniform vec3 u_Color;
 varying vec3 v_color;
 
 void main() {
-    gl_FragColor = vec4(v_color, 1.0);
+    gl_FragColor = vec4(u_Color, 1.0);
 }
 \0";
 
@@ -55,14 +58,18 @@ unsafe fn any_as_f32_slice<T: Sized>(p: &T) -> &[f32] {
 }
 impl Layer for MyLayer {
     fn on_attach(&mut self) {
-        self.triangle_shader = Some(RwLock::new(
-            create_shader(
+        self.triangle_shader = Some(RwLock::new({
+            let mut shader = create_shader(
                 &"Example Triangle".to_string(),
                 &VERTEX_SRC.to_string(),
                 &FRAGMENT_SRC.to_string(),
             )
-            .expect("Could not create triangle shader"),
+            .expect("Could not create triangle shader");
+            shader.add_uniform(&"u_Color".into(), ShaderDataType::Float3).expect("Should be able to add this uniform");
+            shader
+        }
         ));
+
         static VERTEX_DATA: [VertexData; 3] = [
             VertexData {
                 position: glam::vec2(-0.5, -0.5),
@@ -109,7 +116,21 @@ impl Layer for MyLayer {
             .expect("Should have shader by now")
             .read();
         triangle_shader.bind();
+        triangle_shader.set_uniform_fvec3(&"u_Color".into(), &self.color);
         RenderCommand::draw_indexed(&vertex_array);
+    }
+
+    fn imgui_update(&mut self, delta_time: f32, ui : &mut imgui::Ui) {
+        ui.window("Hello Triangle")
+            .size([300.0, 300.0], imgui::Condition::FirstUseEver)
+            .build(||{
+                ui.text("Primera ventana imgui en proto-ecs");
+                let mut triangle_color = [self.color.x, self.color.y, self.color.z];
+                ui.color_picker3("Triangle Color", &mut triangle_color);
+                self.color.x = triangle_color[0];
+                self.color.y = triangle_color[1];
+                self.color.z = triangle_color[2];
+            });
     }
 
     fn on_event(&mut self, event: &mut Event) {}
@@ -119,8 +140,8 @@ fn main() {
     App::initialize();
     WindowManager::init(
         WindowBuilder::new()
-            .with_height(512)
-            .with_width(512)
+            .with_height(720)
+            .with_width(720)
             .with_title("Sandbox Testing".to_owned()),
         Platforms::Windows,
     );
@@ -129,6 +150,7 @@ fn main() {
     App::add_layer(Box::new(MyLayer {
         triangle_shader: None,
         triangle_data: None,
+        color: glam::Vec3 { x: 1.0, y: 1.0, z: 1.0 }
     }));
 
     App::run_application();
