918b8853048d9e5824292ca14a92fe107dbdda07
Luis Diaz <41093870+LDiazN@users.noreply.github.com>
2024-08-21 15:00:34 -0400

    Fix default allocator for handles

--- proto_ecs/src/core/rendering/handle.rs
+++ proto_ecs/src/core/rendering/handle.rs
@@ -1,4 +1,4 @@
-use std::{cell::RefCell, mem::MaybeUninit};
+use std::{cell::RefCell, fmt::Debug, mem::MaybeUninit};
 
 use scc::Stack;
 use num::{Integer, Zero};
@@ -17,17 +17,18 @@ pub struct GenericHandle<IndexType, GenType>
 
 pub type Handle = GenericHandle<u32, u32>;
 
-pub trait IsHandle : Clone + Copy {
-    type Index : Integer + Into<usize> + From<usize> + Clone + Copy; 
-    type Generation : Integer + Zero + Clone + Copy; 
+pub trait IsHandle : Clone + Copy + Debug {
+    type Index : Integer + TryInto<usize> + TryFrom<usize> + Clone + Copy + Debug; 
+    type Generation : Integer + Zero + Clone + Copy + Debug; 
     fn index(&self) -> Self::Index;
     fn generation(&self) -> Self::Generation;
     fn new(index : Self::Index, generation : Self::Generation) -> Self;
+    fn array_index(&self) -> usize;
 }
 
 impl<IndexType, GenType> IsHandle for GenericHandle<IndexType, GenType> 
-    where  IndexType : Integer + Into<usize> + From<usize> + Clone + Copy,
-            GenType : Integer + Clone + Copy
+    where  IndexType : Integer + TryInto<usize> + TryFrom<usize> + Clone + Copy + Debug,
+            GenType : Integer + Clone + Copy + Debug
 {
     type Generation = GenType;
     type Index = IndexType;
@@ -46,8 +47,20 @@ impl<IndexType, GenType> IsHandle for GenericHandle<IndexType, GenType>
     fn index(&self) -> Self::Index {
         self.index
     }
+
+    fn array_index(&self) -> usize {
+        unsafe {
+            self.index().try_into().unwrap_unchecked()
+        }
+    }
 }
 
+/// Use this allocator in most cases. If you really need 
+/// one with a specific type of pointer, you can use [GenerationalIndexAllocator] with 
+/// your specific type of Handle
+pub type Allocator<V> = GenerationalIndexAllocator<Handle, V>;
+
+/// Basic allocator type that can work for most cases
 pub struct GenerationalIndexAllocator <K : IsHandle, V>
 {
     free: Stack<usize>,
@@ -59,7 +72,11 @@ struct AllocatorEntry<V, G> {
     generation : G
 }
 
-impl<K : IsHandle, V> Default for GenerationalIndexAllocator<K,V> {
+// TODO Actual send + sync implementation
+unsafe impl<K : Send + IsHandle, V : Send> Send for GenerationalIndexAllocator<K, V> {}
+unsafe impl<K : Sync + IsHandle, V : Sync> Sync for GenerationalIndexAllocator<K, V> {}
+
+impl<K : IsHandle + Debug, V> Default for GenerationalIndexAllocator<K,V> {
     fn default() -> Self {
         Self::new()
     }
@@ -82,19 +99,24 @@ impl<K : IsHandle, V> GenerationalIndexAllocator<K, V> {
             self.entries.push(AllocatorEntry{value: RefCell::new(MaybeUninit::new(value)), generation : <K as IsHandle>::Generation::zero()});
 
             // Create handle:
-            return  K::new(K::Index::from(next_index), K::Generation::zero());
+            let index = unsafe {
+                // This will only crash in non 32 or 64 bits architectures
+                K::Index::try_from(next_index).unwrap_unchecked()
+            };
+            return  K::new(index, K::Generation::zero());
         }
 
         let next_index = **self.free.pop().unwrap();
         let next_generation = self.entries[next_index].generation;
         self.entries[next_index].value.borrow_mut().write(value);
 
-        K::new(next_index.into(), next_generation)
+        let index = unsafe {next_index.try_into().unwrap_unchecked()};
+        K::new(index, next_generation)
     }
 
     #[inline(always)]
     fn is_live(&self, key : K) -> bool {
-        let index : usize = key.index().into();
+        let index : usize = key.array_index();
         self.entries[index].generation == key.generation()
     }
 
@@ -102,7 +124,7 @@ impl<K : IsHandle, V> GenerationalIndexAllocator<K, V> {
         debug_assert!(self.is_live(key), "Trying to access dead handle");
 
         // Reset the entry
-        let index : usize = key.index().into();
+        let index : usize = key.array_index();
         unsafe {
             self.entries[index].value.borrow_mut().assume_init_drop();
         }
@@ -114,7 +136,7 @@ impl<K : IsHandle, V> GenerationalIndexAllocator<K, V> {
 
     fn get(&self, key : K) -> &mut V {
         debug_assert!(self.is_live(key), "Trying to access dead handle");
-        let index : usize = key.index().into();
+        let index : usize = key.array_index();
         let entry = self.entries[index];
         return unsafe {
             entry.value.borrow_mut().as_mut_ptr().as_mut().unwrap()
