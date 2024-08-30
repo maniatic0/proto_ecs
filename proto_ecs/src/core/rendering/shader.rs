d2779d13ccba000441a96dceff58d74797a28c4b
Luis Diaz <41093870+LDiazN@users.noreply.github.com>
2024-08-29 21:13:17 -0400

    Add more size of types for shader data types

--- proto_ecs/src/core/rendering/shader.rs
+++ proto_ecs/src/core/rendering/shader.rs
@@ -1,17 +1,37 @@
 /// Possible uniform data types
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[allow(non_camel_case_types)]
 pub enum ShaderDataType {
     None,
-    Float,
-    Float2,
-    Float3,
-    Float4,
-    Mat3,
-    Mat4,
-    Int,
-    Int2,
-    Int3,
-    Int4,
+
+    Float_16,
+    Float2_16,
+    Float3_16,
+    Float4_16,
+
+    Float_32,
+    Float2_32,
+    Float3_32,
+    Float4_32,
+
+    Mat3_32,
+    Mat4_32,
+
+    Int_8,
+    Int2_8,
+    Int3_8,
+    Int4_8,
+
+    Int_16,
+    Int2_16,
+    Int3_16,
+    Int4_16,
+
+    Int_32,
+    Int2_32,
+    Int3_32,
+    Int4_32,
+
     Bool,
 }
 
@@ -20,12 +40,23 @@ impl ShaderDataType {
     pub fn get_size(&self) -> u32 {
         match self {
             ShaderDataType::None => 0,
-            ShaderDataType::Float | ShaderDataType::Int => 4,
-            ShaderDataType::Float2 | ShaderDataType::Int2 => 2 * 4,
-            ShaderDataType::Float3 | ShaderDataType::Int3 => 3 * 4,
-            ShaderDataType::Float4 | ShaderDataType::Int4 => 4 * 4,
-            ShaderDataType::Mat3 => 3 * 3 * 4,
-            ShaderDataType::Mat4 => 4 * 4 * 4,
+            ShaderDataType::Float_32 | ShaderDataType::Int_32 => 4,
+            ShaderDataType::Float2_32 | ShaderDataType::Int2_32 => 2 * 4,
+            ShaderDataType::Float3_32 | ShaderDataType::Int3_32 => 3 * 4,
+            ShaderDataType::Float4_32 | ShaderDataType::Int4_32 => 4 * 4,
+
+            ShaderDataType::Float_16 | ShaderDataType::Int_16 => 2,
+            ShaderDataType::Float2_16 | ShaderDataType::Int2_16 => 2 * 2,
+            ShaderDataType::Float3_16 | ShaderDataType::Int3_16 => 3 * 2,
+            ShaderDataType::Float4_16 | ShaderDataType::Int4_16 => 4 * 2,
+
+            ShaderDataType::Int_8 => 1,
+            ShaderDataType::Int2_8 => 2 * 1,
+            ShaderDataType::Int3_8 => 3 * 1,
+            ShaderDataType::Int4_8 => 4 * 1,
+
+            ShaderDataType::Mat3_32 => 3 * 3 * 4,
+            ShaderDataType::Mat4_32 => 4 * 4 * 4,
             ShaderDataType::Bool => 1,
         }
     }
