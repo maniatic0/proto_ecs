3b73c808c3d32fad1ee828cd88b811f83b32bc31
Luis Diaz <41093870+LDiazN@users.noreply.github.com>
2024-08-29 22:41:45 -0400

    Initial material data type

---
+++ proto_ecs/src/core/rendering/material.rs
@@ -0,0 +1,21 @@
+use super::{render_api::ShaderHandle, shader::ShaderDataTypeValue};
+use std::collections::HashMap;
+
+type MaterialArguments = HashMap<String, ShaderDataTypeValue>;
+
+pub struct Material {
+    pub(crate) shader: ShaderHandle,
+    parameters: MaterialArguments,
+}
+
+impl Material {
+
+    /// Set a parameter for the shader in this material. The existence of the parameter 
+    /// is not checked in this function, but when this material gets actually used in a shader
+    pub fn set_parameter(&mut self, parameter: &str, value: ShaderDataTypeValue) {
+        self.parameters
+            .entry(parameter.into())
+            .and_modify(|old_value| *old_value = value.clone())
+            .or_insert(value);
+    }
+}
